11953966b2440065770ff29581b2b7517cfed571
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-08-10 12:15:18 +0300

    init project with mongodb and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose');
+
+const DB_HOST =
+  'mongodb+srv://contacts_db_admin:UjVSgP1kAkCISOQK@cluster0.hguz3za.mongodb.net/db-contacts?retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch(e => {
+    console.log(e.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log('Server running. Use our API on port: 3000');
+// });
